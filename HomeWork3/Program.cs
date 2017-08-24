b97565922fb4ddb7eec0670dbca7005c010a0b5d
Станислав <@->
2017-08-24 17:09:50 +0300

    HomeWork3

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -10,6 +10,83 @@ namespace HomeWork3
     {
         static void Main(string[] args)
         {
+            /*Задача 4.5
+  В программе определены два числа, определяющие нижний и верхний пороговые уровни на цену товара.
+  Ввести с клавиатуры новую цену товара.
+  Реализовать следующие условия: 
+  если цена поднялась выше верхнего значения, то сообщить об этом трейдеру, 
+  если цена опустилась ниже нежнего порога,  то сообщить об этом трейдеру, 
+  в остальных случаях не осуществлять никаких действий.
+  Предложить два варианта(последовательный и вложенный if) */
+
+            /* 
+              int prL = 10, prU = 20;
+
+              Console.WriteLine("Lower price: " + prL);
+              Console.WriteLine("Upper price: " + prU);
+              Console.Write("What is new price? - ");
+
+              double prN = double.Parse(Console.ReadLine());
+              if (prN < prL)
+              {
+                  Console.WriteLine("\nNew price ({0}) is LESS than Upper price ({1}). Call a trader!", prN, prL);
+              }
+                 else if (prN > prU)
+              {
+                  Console.WriteLine("\nNew price ({0}) is MORE than Upper price ({1}). Call a trader!", prN, prU);
+              }
+              else
+                  {
+                      Console.WriteLine("\nNew price in the limit!");
+                  }
+           
+
+            int prL = 10, prU = 20;
+
+            Console.WriteLine("Lower price: " + prL);
+            Console.WriteLine("Upper price: " + prU);
+            Console.Write("What is new price? - ");
+
+            double prN = double.Parse(Console.ReadLine());
+
+            if (prN < prL || prN > prU)
+            {
+                Console.WriteLine("\nThe new price is NOT in the limit. Call a treder!");
+            }
+            else
+            {
+                Console.WriteLine("\nThe new price in the limit!");
+            }
+            */
+
+            /* Задача 4.6
+  Написать программу -калькулятор.Пользователь вводит два числа и то действие, 
+  которое нужно совершить (+, -, *, /, %)
+  Вывести полученный результат на экран.
+  Реализовать два варианта(с оператором if и case) */
+
+            Console.Write("Enter the number_1: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number_2: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter action:    +   -   *   /   : ");
+            char act = char.Parse(Console.ReadLine());
+
+            if (act == +)
+            {
+                Console.WriteLine("{0} + {1} = {2}", n1, n2, (n1 + n2));
+            }
+                    
+                    
+            
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
